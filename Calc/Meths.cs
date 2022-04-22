1e4f8c24e5f0b439f77828619d211a0eb396555a
fokolekose <kuxnin97@gmail.com>
2022-04-22 19:19:45 +0300

    Дз

---
+++ Calc/Meths.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Calc
+{
+    internal class Meths
+    {
+        private int _number;
+
+        public Meths(int number)
+        {
+            _number = number;
+        }
+
+        public string NumberFact { get { return $"{Factorial(_number)}"; } }
+        public string NumberSum { get { return $"{Sum(_number)}"; } }
+        public string NumberMaxEven { get { return $"{MaxEven(_number)}"; } }
+
+        private int Factorial(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        private int Sum(int n)
+        {
+            int result = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+
+        private int MaxEven(int n)
+        {
+            int result = 0;
+            for(var i = 1; i <= n; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    result = i;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Calc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calc
+{
+    internal class Calculator
+    {
+        public static void Main()
+        {
+            int number;
+
+            Console.Write("Здравствуйте вас приветствует математическая программа.\nПожалуйста, введите число: ");
+            string s = Console.ReadLine();
+            if (s == "q")
+            {
+                return;
+            }
+            else number = Int32.Parse(s);
+
+            Meths meth = new Meths(number);
+
+            Console.WriteLine($"Факториал числа {number} = {meth.NumberFact}");
+            Console.WriteLine($"Сумма чисел от 1 до {number} = {meth.NumberSum}");
+            Console.WriteLine($"Максимальное четное число меньше {number} = {meth.NumberMaxEven}");
+        }
+    }
+}
